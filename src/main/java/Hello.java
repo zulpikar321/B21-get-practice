2f034410b77b62e4cb5b27965979ee8218b97066
zulpikar321 <zulpikarzulay@gmail.com>
2021-01-27 01:08:32 -0800

    B21 learning Git

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        System.out.println("B21 future SDET learning Git");
     }
 
 }
