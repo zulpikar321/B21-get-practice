1c989756c4982488d65e2e02e2c7c77e92285597
zulpikar321 <zulpikarzulay@gmail.com>
2021-01-28 23:24:17 -0800

    movie 1 branch updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+        System.out.println("Hello Word, B21");
         System.out.println("B21 future SDET learning Git");
     }
 
