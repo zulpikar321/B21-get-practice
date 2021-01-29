512214b96b0502138e6b6ecb61af1fe40348a334
zulpikar321 <zulpikarzulay@gmail.com>
2021-01-28 23:44:53 -0800

    new branch created

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Word B22");
         System.out.println("B21 future SDET learning Git");
+
+        //watching TV
     }
 
 }
