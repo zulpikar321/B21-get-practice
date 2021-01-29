6aa6c5f0d20b58c477b5819657e8f3fe63493aa7
zulpikar321 <zulpikarzulay@gmail.com>
2021-01-28 23:26:05 -0800

    movie 1 branch more work here

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Word, B21");
         System.out.println("B21 future SDET learning Git");
+        // watching more movie
     }
 
 }
