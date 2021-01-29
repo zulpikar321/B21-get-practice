cf8db9d60e83faae4d67795c1337b43107a137ba
zulpikar321 <zulpikarzulay@gmail.com>
2021-01-28 22:20:25 -0800

    more work on featureA on new branch

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -3,6 +3,7 @@ public class FeatureACode {
     public static void main(String[] args) {
 
         System.out.println("Awesome word for A started");
+        System.out.println("B21 is learning more git with review");
 
     }
 
